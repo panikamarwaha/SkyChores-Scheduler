f8807c63b214753584b208abbb8210c6c3004095
Panika Marwaha <101396699+panikamarwaha@users.noreply.github.com>
2023-10-28 18:37:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+// import './App.css';
+// import TodoForm from './components/TodoForm';
+
+// function App() {
+//   return (
+//     <div className="todo-app">
+//      <h1>Todo App</h1>
+//      <TodoForm />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
